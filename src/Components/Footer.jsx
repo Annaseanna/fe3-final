2bfadb09d83cc69ed4318d170db582dcb6b1e555
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-12-10 09:11:03 -0500

    Subiendo el proyecto

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import facebook from '../images/ico-facebook.png'
+import instagram from '../images/ico-instagram.png'
+import tiktok from '../images/ico-tiktok.png'
+import whasapp from '../images/ico-whatsapp.png'
+import { useGlobalStates } from './utils/global.context'
 
-const Footer = () => {
+const Footer= () => {
+
+  const{state}=useGlobalStates()
   return (
-    <footer>
+    <footer className={state.theme}>
+        <div className='flex'>
         <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+        <img src="https://images.prismic.io/dh-frontend/3daea5ec-e5b0-451a-a27e-2b039506c9ca_DigitalHouse_Logo_Black.png?auto=compress,format" alt='DH-logo' />
+        </div>
+        <div className='logos'>
+          <img src={facebook} alt="logoFace" />
+          <img src={instagram} alt="logoInsta" />
+          <img src={tiktok} alt="logoTik" />
+          <img src={whasapp} alt="logoWsp" />
+        </div>
     </footer>
   )
 }
 
-export default Footer
